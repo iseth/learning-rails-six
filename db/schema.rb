a0af29d0254314035983a46f4c4150dbf18da491
Seth Horsley <seth@devduo.com>
2020-05-06 14:31:35 -0400

    Added users

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_181033) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
