2cce2c3cb5bfd93c2447a7174c988ca03bbc62be
Seth Horsley <seth@devduo.com>
2020-05-06 14:56:00 -0400

    Added associations to user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was saved."
       redirect_to @article
